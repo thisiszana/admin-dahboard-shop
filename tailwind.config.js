9855ef1452a6147600d10d3e2a203492b836cdf7
Zana Amanallhy <zanaamanallhy80@gmail.com>
2024-08-02 21:01:19 +0330

    Initial implementation of the project and creation of necessary program folders. Install required libraries. Customize tailwindcss styles. Add a series of custom styles to the global.css file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,185 @@
+const { nextui } = require("@nextui-org/react");
+import colors from "tailwindcss/colors";
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@tremor/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    transparent: "transparent",
+    current: "currentColor",
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        lightOrange: "#ffedd5",
+        lightRose: "#ffe4e6",
+        lightPurple: "#ede9fe",
+        lightGray: "#f3f4f6",
+        lightSlate: "#cbd5e1",
+        lightBlue: "#dbeafe",
+        lightGreen: "#dcfce7",
+        darkOrange: "#c2410c",
+        darkRose: "#e11d48",
+        darkPurple: "#7c3aed",
+        darkGray: "#6b7280",
+        darkSlate: "#64748b",
+        darkBlue: "#2563eb",
+        darkGreen: "#22c55e",
+        dark1: "#212b36",
+        dark2: "#232b33",
+        // base color:
+        baseLight: "#ede9fe",
+        baseDark: "#7c3aed",
+        tremor: {
+          brand: {
+            faint: colors.purple[50],
+            muted: colors.purple[200],
+            subtle: colors.purple[400],
+            DEFAULT: "#7c3aed",
+            emphasis: colors.purple[700],
+            inverted: colors.white,
+          },
+          background: {
+            muted: colors.gray[50],
+            subtle: colors.gray[100],
+            DEFAULT: colors.white,
+            emphasis: colors.gray[700],
+          },
+          border: {
+            DEFAULT: colors.gray[200],
+          },
+          ring: {
+            DEFAULT: colors.gray[200],
+          },
+          content: {
+            subtle: colors.gray[400],
+            DEFAULT: colors.gray[500],
+            emphasis: colors.gray[700],
+            strong: colors.gray[900],
+            inverted: colors.white,
+          },
+        },
+      },
+      padding: {
+        box: "25px 20px",
+        btn: "8px 20px",
+        input: "16.5px 14px",
+      },
+      borderRadius: {
+        box: "10px",
+        btn: "8px",
+        input: "10px",
+        "tremor-small": "0.375rem",
+        "tremor-default": "0.5rem",
+        "tremor-full": "9999px",
+      },
+      fontSize: {
+        base: "16px",
+        h1: "30px",
+        h2: "25px",
+        h3: "20px",
+        h4: "17px",
+        p1: "14px",
+        p2: "12px",
+        p3: "10px",
+        "tremor-label": ["0.75rem", { lineHeight: "1rem" }],
+        "tremor-default": ["0.875rem", { lineHeight: "1.25rem" }],
+        "tremor-title": ["1.125rem", { lineHeight: "1.75rem" }],
+        "tremor-metric": ["1.875rem", { lineHeight: "2.25rem" }],
+      },
+      gap: {
+        btn: "12px",
+        box: "25px",
+      },
+      borderColor: {
+        inputLight: "#e5e7eb",
+        inputDark: "#6b7280",
+      },
+      boxShadow: {
+        // light
+        "tremor-input": "0 1px 2px 0 rgb(0 0 0 / 0.05)",
+        "tremor-card":
+          "0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)",
+        "tremor-dropdown":
+          "0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)",
+        // dark
+        "dark-tremor-input": "0 1px 2px 0 rgb(0 0 0 / 0.05)",
+        "dark-tremor-card":
+          "0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)",
+        "dark-tremor-dropdown":
+          "0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)",
       },
     },
   },
-  plugins: [],
+  safelist: [
+    {
+      pattern:
+        /^(bg-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/,
+      variants: ["hover", "ui-selected"],
+    },
+    {
+      pattern:
+        /^(text-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/,
+      variants: ["hover", "ui-selected"],
+    },
+    {
+      pattern:
+        /^(border-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/,
+      variants: ["hover", "ui-selected"],
+    },
+    {
+      pattern:
+        /^(ring-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/,
+    },
+    {
+      pattern:
+        /^(stroke-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/,
+    },
+    {
+      pattern:
+        /^(fill-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/,
+    },
+    ...[
+      "[#ffedd5]",
+      "[#ffe4e6]",
+      "[#ede9fe]",
+      "[#f3f4f6]",
+      "[#cbd5e1]",
+      "[#dbeafe]",
+      "[#dbeafe]",
+      "[#dcfce7]",
+      "[#c2410c]",
+      "[#e11d48]",
+      "[#7c3aed]",
+      "[#6b7280]",
+      "[#64748b]",
+      "[#2563eb]",
+      "[#22c55e]",
+      "[#212b36]",
+      "[#232b33]",
+      "[#ede9fe]",
+      "[#7c3aed]",
+    ].flatMap((customColor) => [
+      `bg-${customColor}`,
+      `border-${customColor}`,
+      `hover:bg-${customColor}`,
+      `hover:border-${customColor}`,
+      `hover:text-${customColor}`,
+      `fill-${customColor}`,
+      `ring-${customColor}`,
+      `stroke-${customColor}`,
+      `text-${customColor}`,
+      `ui-selected:bg-${customColor}`,
+      `ui-selected:border-${customColor}`,
+      `ui-selected:text-${customColor}`,
+    ]),
+  ],
+  plugins: [
+    nextui(),
+    require("@headlessui/tailwindcss"),
+  ],
 };
