ef3c0b78895fc965bb7bde03b1226b9ca44d0934
Zana Amanallhy <zanaamanallhy80@gmail.com>
2024-08-03 23:59:24 +0330

    Add the admin session to the auth related layout.

--- app/(auth)/layout.jsx
+++ app/(auth)/layout.jsx
@@ -1,3 +1,9 @@
-export default function layout({ children }) {
+import { getServerSession } from "@/utils/session";
+import { redirect } from "next/navigation";
+
+export default function AuthLayout({ children }) {
+  const session = getServerSession();
+
+  if (session) redirect("/dashboard");
   return <main>{children}</main>;
 }
