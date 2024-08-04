0a8e56f83a99997104f3ccb3de2781c0cc853315
Zana Amanallhy <zanaamanallhy80@gmail.com>
2024-08-04 11:55:28 +0330

    Create the uploadImage function to create a link and send the photo to the cloudinary server. and do cloudinary configuration in next.config

--- utils/fun.js
+++ utils/fun.js
@@ -4,3 +4,26 @@ export const hashedPassword = async (password) => await hash(password, 12);
 
 export const verifyPassword = async (password, hashedPassword) =>
   compare(password, hashedPassword);
+export const uploadImage = async (path) => {
+  const cloudName = process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME;
+  const uploadPreset = process.env.NEXT_PUBLIC_UPLOAD_PRESET;
+
+  const url = `https://api.cloudinary.com/v1_1/${cloudName}/image/upload`;
+  const formData = new FormData();
+
+  formData.append("file", path);
+  formData.append("upload_preset", uploadPreset);
+
+  try {
+    const res = await fetch(url, {
+      method: "POST",
+      body: formData,
+    });
+    const data = await res.json();
+    return { imageUrl: data.secure_url };
+  } catch (error) {
+    return {
+      imageUrl: null,
+    };
+  }
+};
