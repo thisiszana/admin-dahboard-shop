ed00d6cd69b58bc1b78ad2486827d96960be0ed4
Zana Amanallhy <zanaamanallhy80@gmail.com>
2024-08-07 09:21:58 +0330

    Implementation of ui/ux button action part for each product.

--- components/pages/products/ui/ProductActions.jsx
+++ components/pages/products/ui/ProductActions.jsx
@@ -1,14 +1,68 @@
 "use client";
 
-import { MenuDots } from "@/components/icons/Icon";
+import { changeProductStatus } from "@/actions/product.action";
+import { Draft, Edit, EyeOpen, MenuDots, Publish } from "@/components/icons/Icon";
 import CustomBtn from "@/components/shared/CustomBtn";
+import Loader from "@/components/shared/Loader";
+import useServerAction from "@/hooks/useCallServerAction";
 import { Popover } from "antd";
+import Link from "next/link";
 import { useState } from "react";
 
 export default function ProductActions({ productId, published }) {
   const [open, setOpen] = useState(false);
 
-//   const content = 
+  const onOpenChange = (newOpen) => {
+    setOpen(newOpen);
+  };
+
+  const { loading, res } = useServerAction(
+    changeProductStatus,
+    {
+      id: productId,
+      published,
+    },
+    () => onOpenChange()
+  );
+
+  const content = loading ? (
+    <div className="w-[150px] h-[160px] flex items-center justify-center">
+      <Loader />
+    </div>
+  ) : (
+    <div className="popContainer min-w-[150px]">
+      <CustomBtn
+        disabled={published || loading}
+        title="Publish"
+        icon={<Publish />}
+        classNames={`popButton ${
+          published ? "text-darkGreen bg-lightGreen" : "hoverable"
+        }`}
+        onClick={res}
+      />
+      <CustomBtn
+        disabled={!published || loading}
+        title="Draft"
+        icon={<Draft />}
+        classNames={`popButton ${
+          !published ? "text-darkOrange bg-lightOrange" : "hoverable"
+        }`}
+        onClick={res}
+      />
+      <hr />
+      <Link href={`/products/${productId}`} className="popButton hoverable">
+        <EyeOpen />
+        Details
+      </Link>
+      <Link
+        href={`/products/edit/${productId}`}
+        className="popButton hoverable"
+      >
+        <Edit />
+        Edit
+      </Link>
+    </div>
+  );
   return (
     <div className="flex gap-2">
       <Popover
