3013ec9b307ee3c83c56029915d5ecff0fefa9fe
Zana Amanallhy <zanaamanallhy80@gmail.com>
2024-09-05 21:15:13 +0330

    debugging ...

--- app/api/(product)/products/[id]/route.js
+++ app/api/(product)/products/[id]/route.js
@@ -30,3 +30,42 @@ export async function GET(req, { params: { id } }) {
     );
   }
 }
+
+export async function POST(req, { params: { id } }) {
+  try {
+    await connectDB();
+  } catch (error) {
+    return NextResponse.json(
+      {
+        msg: "Server error!",
+        success: false,
+      },
+      { status: 500 }
+    );
+  }
+
+  try {
+    const { commentId } = await req.json();
+
+    const product = await ProductAdminSorme.findById(id);
+
+    if (!product)
+      return NextResponse.json(
+        { msg: "Product not found!", success: false },
+        { status: 404 }
+      );
+
+    product.comments.push(commentId);
+    await product.save();
+
+    return NextResponse.json(
+      { msg: "Comment added successfully!", success: true },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { msg: "Server Error!", success: false },
+      { status: 500 }
+    );
+  }
+}
