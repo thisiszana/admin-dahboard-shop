8b4250c96b13546d57c2614b060114da0edda881
Zana Amanallhy <zanaamanallhy80@gmail.com>
2024-09-07 23:49:47 +0330

    Refactoring api route

--- app/api/(product)/products/related/[id]/route.js
+++ app/api/(product)/products/related/[id]/route.js
@@ -1,8 +1,10 @@
+// pages/api/products/related/[id].js
+
 import { ProductAdminSorme } from "@/models/productAdminSorme";
 import connectDB from "@/utils/connectDB";
 import { NextResponse } from "next/server";
 
-export async function POST(req, { params: { id } }) {
+export async function GET(req, { params }) {
   try {
     await connectDB();
   } catch (error) {
@@ -14,9 +16,21 @@ export async function POST(req, { params: { id } }) {
   }
 
   try {
-    const { categoryId } = await req.json();
+    const { id } = params;
+
+    const product = await ProductAdminSorme.findById(id);
+
+    if (!product) {
+      return NextResponse.json(
+        { msg: "Product not found", success: false },
+        { status: 404 }
+      );
+    }
+
+    const productCategory = product.category;
+
     const relatedProducts = await ProductAdminSorme.find({
-      category: categoryId,
+      category: productCategory,
       stock: { $gt: 0 },
       published: true,
     }).lean();
@@ -30,9 +44,10 @@ export async function POST(req, { params: { id } }) {
       { status: 200 }
     );
 
-    response.headers.set("Cache-Control", "no-store");
+    response.headers.append("Cache-Control", "no-store");
     return response;
   } catch (error) {
+    console.log("Error finding related products:", error.message);
     return NextResponse.json(
       { msg: "Server Error!", success: false },
       { status: 500 }
