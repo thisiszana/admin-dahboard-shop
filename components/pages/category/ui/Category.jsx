81ee95e6efcd41ce2ab72e37465a1df30b7c0293
Zana Amanallhy <zanaamanallhy80@gmail.com>
2024-09-16 01:09:39 +0330

    Create category detail page action.

--- components/pages/category/ui/Category.jsx
+++ components/pages/category/ui/Category.jsx
@@ -1,10 +1,10 @@
-import CategoryActions from "../../categories/ui/CategoryActions";
 import CategoryInformation from "./CategoryInformation";
+import CategoryAcyion from "./CategoryAcyion";
 
 export default function Category({ category }) {
   return (
     <div className="flex flex-col gap-box">
-      <CategoryActions id={category?._id} />
+      <CategoryAcyion id={category?._id} />
       <CategoryInformation info={category} />
     </div>
   );
---
+++ components/pages/category/ui/CategoryAcyion.jsx
@@ -0,0 +1,38 @@
+import Link from "next/link";
+
+import { Tooltip } from "antd";
+
+import { EyeOpen, LeftAngle } from "@/components/icons/Icon";
+import CustomLink from "@/components/shared/CustomLink";
+import CustomBtn from "@/components/shared/CustomBtn";
+
+export default function CategoryAcyion() {
+  return (
+    <div className="w-full flex justify-between items-center gap-3">
+      <CustomLink
+        href="/categories"
+        className="backLink"
+        title="Back"
+        icon={<LeftAngle size={10} />}
+      />
+      <div className="flex items-center gap-3">
+        <Tooltip placement="bottom" title="Go Live">
+          <Link href="/" target="_blank">
+            <CustomBtn
+              icon={<EyeOpen className="text-darkGray" />}
+              classNames="iconButton"
+            />
+          </Link>
+        </Tooltip>
+        <Tooltip placement="bottom" title="Edit">
+          <Link href={`/categories/edit/${id}`}>
+            <CustomBtn
+              icon={<Edit className="text-darkGray" />}
+              classNames="iconButton"
+            />
+          </Link>
+        </Tooltip>
+      </div>
+    </div>
+  );
+}
