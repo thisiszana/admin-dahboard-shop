19dc4e2196036ac9d61fa173ebadce760dacb77f
Zana Amanallhy <zanaamanallhy80@gmail.com>
2024-09-26 00:24:02 +0330

    change api config ...

--- configs/api.js
+++ configs/api.js
@@ -1,6 +1,26 @@
-import axios from "axios";
+const axios = require("axios");
 
-const baseURL = process.env.BASE_URL;
+const checkConfig = (server) => {
+  let config = {};
+  switch (server) {
+    case "production":
+      config = {
+        baseURL: "https://admin-dahboard-shop.vercel.app/",
+      };
+      break;
+    case "local":
+      config = {
+        baseURL: "http://localhost:3000",
+      };
+      break;
+    default:
+      break;
+  }
+  return config;
+};
+
+const selectServer = "production";
+const { baseURL } = checkConfig(selectServer);
 
 const headers = {
   "Content-Type": "application/json",
@@ -12,4 +32,4 @@ const api = axios.create({
   timeout: 60000,
 });
 
-export default api;
+module.exports = api;
